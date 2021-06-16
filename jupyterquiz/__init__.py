9d2eda5b6a20201f1dcd01fbce83262b86304522
John M. Shea <jshea@ece.ufl.edu>
2021-06-16 14:50:04 -0400

    Push version number to 1.3.1 to reflect minor fix to MathJax rendering of questions

--- jupyterquiz/__init__.py
+++ jupyterquiz/__init__.py
@@ -11,5 +11,5 @@ for the book Introduction to Data Science for Engineers
 All files in the package are distributed under the MIT License
 '''
 
-__version__ = '1.3'
+__version__ = '1.3.1'
 from .dynamic import  display_quiz
