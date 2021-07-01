ed334720f5890a9fe5af9a7fd860f54b5a8a2658
John M. Shea <jshea@ece.ufl.edu>
2021-07-01 16:44:47 -0400

    Fix an issue where multiple-choice buttons that contain <p> tags would not have the text change color correctly for the parts in the <p>s. Bump minor version number.

--- jupyterquiz/__init__.py
+++ jupyterquiz/__init__.py
@@ -11,5 +11,5 @@ for the book Introduction to Data Science for Engineers
 All files in the package are distributed under the MIT License
 '''
 
-__version__ = '1.5'
+__version__ = '1.5.1'
 from .dynamic import  display_quiz
