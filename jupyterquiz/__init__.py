4884a0db877f719eb340a9ff204f4157d87d427e
John M. Shea <jshea@ece.ufl.edu>
2022-10-02 14:41:38 -0400

    Bump version number for bug fix

--- jupyterquiz/__init__.py
+++ jupyterquiz/__init__.py
@@ -11,5 +11,5 @@ for the book Introduction to Data Science for Engineers
 All files in the package are distributed under the MIT License
 '''
 
-__version__ = '2.0.6'
+__version__ = '2.0.7'
 from .dynamic import display_quiz, capture_responses
