1058e9dd5a08a508e054e68b1324f07608b88820
John M. Shea <jshea@ece.ufl.edu>
2024-01-29 17:12:43 -0500

    Fix font color for multiple-choice buttons to work on Jupyter dark theme.

--- jupyterquiz/__init__.py
+++ jupyterquiz/__init__.py
@@ -11,5 +11,5 @@ for the book Introduction to Data Science for Engineers
 All files in the package are distributed under the MIT License
 '''
 
-__version__ = '2.7.0a1'
+__version__ = '2.7.0a2'
 from .dynamic import display_quiz, capture_responses
