104aef4c098c5d9461806bf86fa9246d5df280cc
John M. Shea <jshea@ece.ufl.edu>
2024-02-11 08:47:50 -0500

    Minor CSS updates to improve readability of <pre> elements in some circumstances

--- jupyterquiz/__init__.py
+++ jupyterquiz/__init__.py
@@ -11,5 +11,5 @@ for the book Introduction to Data Science for Engineers
 All files in the package are distributed under the MIT License
 '''
 
-__version__ = '2.7.0a3'
+__version__ = '2.7.0a4'
 from .dynamic import display_quiz, capture_responses
